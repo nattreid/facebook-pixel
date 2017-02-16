8d977e6b14400d953dd166a0d5b98c329e244a58
attreid <attreid@gmail.com>
2017-02-16 11:34:12 +0100

    pridany metody, uprava readme

--- readme.md
+++ readme.md
@@ -19,7 +19,15 @@ protected function createComponentFacebookPixel() {
 }
 
 public function someRender(){
-    $this['facebookPixel']->search('searchWord');
+    $this['facebookPixel']->search('searchWord');       // vyhledavani
+    $this['facebookPixel']->viewContent(3.5, 'CZK');    // detail
+    $this['facebookPixel']->addToCart(3.5, 'CZK');      // pridani do kosiku
+    $this['facebookPixel']->addToWishList(3.5, 'CZK');  // pridani do prani
+    $this['facebookPixel']->initiateCheckout();         // prechod k zaplaceni nakupu
+    $this['facebookPixel']->addPaymentInfo();           // pridani platebnich udaju
+    $this['facebookPixel']->purchase(3.5, 'CZK');       // provedeni nakupu
+    $this['facebookPixel']->lead();                     // potencialni zakaznik
+    $this['facebookPixel']->completeRegistration();     // dokonceni registrace
 }
 ```
 
@@ -30,5 +38,10 @@ v latte
     <!-- html kod -->
     {control facebookPixel}
 </head>
+<body>
+    {control facebookPixel:ajax}
+    <!-- html kod -->
+</body>
+</html>
 
 ```
--- src/FacebookPixel.php
+++ src/FacebookPixel.php
@@ -15,17 +15,155 @@ class FacebookPixel extends Control
 	/** @var string */
 	private $apiKey;
 
+	/** @var string[][] */
+	private $events = [];
+
+	/** @var string[][] */
+	private $ajaxEvents = [];
+
+	/** @var string */
+	private $latte = 'default';
+
 	public function __construct($apiKey)
 	{
 		parent::__construct();
 		$this->apiKey = $apiKey;
 	}
 
+	/**
+	 * Search event
+	 * @param string $searchString
+	 */
+	public function search($searchString)
+	{
+		$this->events['Search'] = [
+			'search_string' => $searchString
+		];
+	}
+
+	/**
+	 * View Content (Detail) event
+	 * @param float $value
+	 * @param string $currency
+	 */
+	public function viewContent($value = null, $currency = null)
+	{
+		$values = [];
+		if ($value !== null) {
+			$values['value'] = $value;
+		}
+		if ($currency !== null) {
+			$values['currency'] = $currency;
+		}
+
+		$this->events['ViewContent'] = $values;
+	}
+
+	/**
+	 * Add to Cart event
+	 * @param float $value
+	 * @param string $currency
+	 */
+	public function addToCart($value = null, $currency = null)
+	{
+		$values = [];
+		if ($value !== null) {
+			$values['value'] = $value;
+		}
+		if ($currency !== null) {
+			$values['currency'] = $currency;
+		}
+
+		$this->ajaxEvents['AddToCart'] = $values;
+		$this->redrawControl('ajaxEvents');
+		$this->latte = 'ajax';
+	}
+
+	/**
+	 * Add to Wish List event
+	 * @param float $value
+	 * @param string $currency
+	 */
+	public function addToWishList($value = null, $currency = null)
+	{
+		$values = [];
+		if ($value !== null) {
+			$values['value'] = $value;
+		}
+		if ($currency !== null) {
+			$values['currency'] = $currency;
+		}
+
+		$this->ajaxEvents['AddToWishList'] = $values;
+		$this->redrawControl('ajaxEvents');
+		$this->latte = 'ajax';
+	}
+
+	/**
+	 * Initiate Checkout event
+	 */
+	public function initiateCheckout()
+	{
+		$this->events['InitiateCheckout'] = [];
+	}
+
+	/**
+	 * Add Payment Info event
+	 */
+	public function addPaymentInfo()
+	{
+		$this->events['AddPaymentInfo'] = [];
+	}
+
+	/**
+	 * Purchase event
+	 * @param float $value
+	 * @param string $currency
+	 */
+	public function purchase($value = null, $currency = null)
+	{
+		$values = [];
+		if ($value !== null) {
+			$values['value'] = $value;
+		}
+		if ($currency !== null) {
+			$values['currency'] = $currency;
+		}
+
+		$this->ajaxEvents['Purchase'] = $values;
+		$this->redrawControl('ajaxEvents');
+		$this->latte = 'ajax';
+	}
+
+	/**
+	 * Lead event (customer)
+	 */
+	public function lead()
+	{
+		$this->events['Lead'] = [];
+	}
+
+	/**
+	 * Complete Registration event (customer)
+	 */
+	public function completeRegistration()
+	{
+		$this->events['CompleteRegistration'] = [];
+	}
+
 	public function render()
 	{
 		$this->template->apiKey = $this->apiKey;
-		$this->template->setFile(__DIR__ . "/templates/default.latte");
+		$this->template->events = $this->events;
+		$this->template->ajaxEvents = $this->ajaxEvents;
+		$this->template->setFile(__DIR__ . "/templates/$this->latte.latte");
 		$this->template->render();
+	}
+
+	public function renderAjax()
+	{
+		$this->latte = 'ajax';
+		$this->render();
 	}
 }
 
